871af3d2b8c22efea4783bcdd5e63228f4ff73c5
igofftt <igorianru@gmail.com>
2017-07-06 00:50:35 +0300

    fa icon libs uplod img

--- app/assets/public/js/admin/custom.js
+++ app/assets/public/js/admin/custom.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
-	// $('.i-checks').iCheck({
-	// 	checkboxClass: 'icheckbox_square-green',
-	// 	radioClass: 'iradio_square-green',
-	// });
+	$('.iCheck').iCheck({
+		checkboxClass: 'icheckbox_square-green',
+		radioClass   : 'iradio_square-green',
+	});
+
 	$(".select2").select2();
 });
