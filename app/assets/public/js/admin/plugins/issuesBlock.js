2f7ead6dd337e46a274fb455476e0065daf02606
igofftt <igorianru@gmail.com>
2017-08-12 17:05:06 +0300

    jqueryUI

--- app/assets/public/js/admin/plugins/issuesBlock.js
+++ app/assets/public/js/admin/plugins/issuesBlock.js
@@ -1,8 +1,30 @@
 (function() {
 	$.isbl = {
+		callbackBgDown: function() {
+			$('[data-block]').each(function(i, el) {
+				$(el).children().each(function(indx, element) {
+					$(element).attr({'data-id': indx + 1, style: ''});
+				})
+			})
+		},
+
 		initialize: function initialize(data) {
 			this.conf = new Map((data || {}).pram);
 			this.setStyle();
+			$.isbl.loadOnclick();
+		},
+
+		loadOnclick: function() {
+			$('[data-block]').sortable({revert: true, update: this.callbackBgDown});
+
+			$('[data-id]')
+				.draggable({
+					appendTo         : 'body',
+					clone            : false,
+					connectToSortable: '[data-block]',
+					revert           : 'invalid',
+					scroll           : false,
+				});
 		},
 
 		setStyle: function() {
