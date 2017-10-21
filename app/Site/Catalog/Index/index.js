f014fb3842f7e19f102b5b8c890bcd250894d0ea
igofftt <igorianru@gmail.com>
2017-10-21 20:16:35 +0300

    showing filters #13

--- app/Site/Catalog/Index/index.js
+++ app/Site/Catalog/Index/index.js
@@ -144,14 +144,15 @@ const
 
 		const
 			renderPage = () => res.render('site/Catalog/indexCatalog', {
-				brand   : req.store.getState('site.brand'),
-				category: req.params.id,
-				error   : req.flash('error').toString(),
-				filters : req.store.getState('site.filters'),
-				menu    : req.store.getState('site.menu'),
-				menuTop : req.store.getState('site.menuTop'),
-				meta    : {title: 'VSPN'},
-				page    : query.page || 1,
+				brand          : req.store.getState('site.brand'),
+				category       : req.params.id,
+				characteristics: JSON.stringify(req.store.getState('site.characteristics')),
+				error          : req.flash('error').toString(),
+				filters        : req.store.getState('site.filters'),
+				menu           : req.store.getState('site.menu'),
+				menuTop        : req.store.getState('site.menuTop'),
+				meta           : {title: 'VSPN'},
+				page           : query.page || 1,
 
 				// TODO move to database
 				params_name  : paramsNames[_.get(req.store.getState('site.filters'), 'params.name')],
@@ -182,11 +183,15 @@ const
 				.findAll({order: [['id', 'ASC']], raw: true})
 				.then(dataObl => req.store.setState('site.brand', dataObl, getMenuTop)),
 
-			getMenu = callback => models.menuModel
-				.findAll({order: [['id', 'ASC']], raw: true})
-				.then(dataObl => req.store.setState('site.menu', dataObl, callback));
+			getCharacteristics = () => models.characteristicsModel
+				.findAll({raw: true, where: {active: 1}})
+				.then(dataObl => req.store.setState('site.characteristics', dataObl, getBrand)),
 
-		return getMenu(getBrand);
+			getMenu = () => models.menuModel
+				.findAll({order: [['id', 'ASC']], raw: true})
+				.then(dataObl => req.store.setState('site.menu', dataObl, getCharacteristics));
+
+		return getMenu();
 	},
 
 	/**
