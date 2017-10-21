0cef2941ff138847ac38e9ccb98a1987b2bce06d
igofftt <igorianru@gmail.com>
2017-10-21 17:26:27 +0300

    meta & metrika #16

--- app/Site/Main/Index/index.js
+++ app/Site/Main/Index/index.js
@@ -55,18 +55,36 @@ const
 					'block_3_2': 'fig_square color-white',
 				},
 
-				menuTop      : req.store.getState('site.menuTop'),
-				meta         : {title: 'VSPN'},
+				menuTop: req.store.getState('site.menuTop'),
+
+				meta: {
+					description: req.store.getState('site.description_main_page').text,
+					title      : req.store.getState('site.title_main_page').text,
+				},
+
+				metrika      : req.store.getState('site.metrika').text,
 				parent_module: 'indexPage',
 				this_module  : 'indexPage',
 				user         : req.user,
 			}),
 
+			getMetrika = () => models.paramsModel.findOne({raw: true, where: {system_name: 'metrika'}})
+				.then(dataObl => req.store.setState('site.metrika', dataObl, () => renderPage())),
+
 			getMainPage = () => models.paramsModel.findOne({raw: true, where: {system_name: 'main_page'}})
-				.then(dataObl => req.store.setState('site.main_page', dataObl, () => renderPage())),
+				.then(dataObl => req.store.setState('site.main_page', dataObl, () => getMetrika())),
+
+			getTitleMainPage = () => models.paramsModel.findOne({raw: true, where: {system_name: 'title_main_page'}})
+				.then(dataObl => req.store.setState('site.title_main_page', dataObl, () => getMainPage())),
+
+			getDescriptionMainPage = () => models.paramsModel.findOne({
+				raw  : true,
+				where: {system_name: 'description_main_page'},
+			})
+				.then(dataObl => req.store.setState('site.description_main_page', dataObl, () => getTitleMainPage())),
 
 			getMenuTop = () => getCat({lang: 'ru', req, res, type: 'array'}, tree =>
-				req.store.setState('site.menuTop', tree, getMainPage));
+				req.store.setState('site.menuTop', tree, getDescriptionMainPage));
 
 		return getMenuTop(renderPage);
 	};
